0bac95390884c98d4b1eb8d76b0a007e37bd656d
Konstanty <Kostu96@gmail.com>
2022-03-24 20:22:35 +0100

    WIP

--- tools/bootloader_generator/main.cpp
+++ tools/bootloader_generator/main.cpp
@@ -1,3 +1,5 @@
+
+#include <cpp-common/helper_functions.h>
 #include <cstdint>
 #include <filesystem>
 #include <fstream>
@@ -10,31 +12,38 @@ namespace fs = std::filesystem;
 
 int main(int argc, char* argv[])
 {
-    if (argc < 4)
-        return -1;
+    constexpr size_t BOOTLOADER_SIZE = 256;
 
-    std::ifstream bootloaderFile{ argv[1], std::ios::binary };
-    if (!bootloaderFile.is_open())
+    if (argc < 4) {
+        std::cerr << "Failed!\n";
         return -1;
+    }
 
-    bootloaderFile.ignore(std::numeric_limits<std::streamsize>::max());
-    size_t bootloadeSize = bootloaderFile.gcount();
-    if (bootloadeSize != 256)
+    uint8_t bootloaderData[BOOTLOADER_SIZE];
+    size_t bootloaderSize = BOOTLOADER_SIZE;
+    if (!readFile(argv[1], (char*)bootloaderData, bootloaderSize, true)) {
+        std::cerr << "Failed!\n";
         return -1;
-    bootloaderFile.clear();
-    bootloaderFile.seekg(std::ios::beg);
+    }
+    if (bootloaderSize != BOOTLOADER_SIZE) {
+        std::cerr << "Failed!\n";
+        return -1;
+    }
+    std::cout << "Loaded bootloader file: " << argv[1] << '\n';
 
     fs::path patchesDir = argv[3];
     std::unordered_map<uint16_t, std::vector<uint8_t>> patches;
     for (auto& dirEntry : fs::directory_iterator{ patchesDir })
         if (dirEntry.is_regular_file())
         {
-            std::cout << "Found patch file: " << dirEntry.path() << '\n';
+            std::cout << "Found patch file: " << dirEntry.path() << "\nLoading... ";
 
             std::ifstream patchFile{ dirEntry.path() };
 
-            if (!patchFile.is_open())
+            if (!patchFile.is_open()) {
+                std::cerr << "Failed!\n";
                 return -1;
+            }
 
             std::string line;
             uint16_t address;
@@ -48,38 +57,45 @@ int main(int argc, char* argv[])
                     data.push_back(byte & 0xFF);
 
                 patches.insert({ address, std::move(data) });
-            } 
+            }
+
+            std::cout << "Success.\n";
         }
+
+    std::cout << "Applying patches... ";
+    for (const auto& patch : patches) {
+        uint16_t address = patch.first;
+        for (auto byte : patch.second) {
+            if (address >= BOOTLOADER_SIZE) {
+                std::cerr << "Failed!\n";
+                return -1;
+            }
+
+            bootloaderData[address++] = byte;
+        }
+    }
+    std::cout << "Done.\n";
+
+    std::cout << "Generatgin output file... ";
 
     std::ofstream outputFile{ argv[2] };
-    if (!outputFile.is_open())
+    if (!outputFile.is_open()) {
+        std::cerr << "Failed!\n";
         return -1;
+    }
 
     outputFile << "static const uint8_t internalROM[256] = {";
-
     outputFile << std::hex << std::setfill('0');
-    uint8_t byte;
-    for (uint16_t i = 0; i < 256; ++i)
-    {
-        if (patches.contains(i)) {
-            auto& patch = patches[i];
-            for (size_t x = 0; x < patch.size(); ++x) {
-                if (i % 16 == 0) outputFile << "\n\t";
-                outputFile << "0x" << std::setw(2) << (uint16_t)patch[x];
-                if (i != 255) outputFile << ", ";
-                if (x < patch.size() -1) ++i;
-                bootloaderFile.ignore();
-            }
-        }
-        else {
-            bootloaderFile.read((char*)&byte, 1);
-            if (i % 16 == 0) outputFile << "\n\t";
-            outputFile << "0x" << std::setw(2) << (uint16_t)byte;
-            if (i != 255) outputFile << ", ";
-        }
+
+    for (uint16_t i = 0; i < BOOTLOADER_SIZE; ++i) {
+        if (i % 16 == 0) outputFile << "\n\t";
+        outputFile << "0x" << std::setw(2) << (uint16_t)bootloaderData[i];
+        if (i != 255) outputFile << ", ";
     }
 
     outputFile << "\n};\n";
+
+    std::cout << "Done.\n";
 
     return 0;
 }
